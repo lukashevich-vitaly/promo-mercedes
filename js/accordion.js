a8bddb5355744913b19598398f001cff37ef37a7
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-09-22 20:48:28 +0300

    fix: fix accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,17 @@
 const lists = document.querySelectorAll('.feature-sub')
 const btns = document.querySelectorAll('.feature__link')
 
-//btns.addEventListener('click', () => alert('Clicked button'));
-
-/*for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', () => alert('Clicked button'));
-}*/
-
-btns.forEach((btnItem, index) => {
-    btnItem.addEventListener('click', () => {
-        
-        btns.forEach((btnItem) => {
-            btnItem.classList.remove('feature__link_active')
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btns.forEach((btnItem, idx) => {
+            if (btnItem === btn) {
+                btnItem.classList.toggle('feature__link_active')
+                lists[idx].classList.toggle('hidden')
+            } else {
+                btnItem.classList.remove('feature__link_active')
+                lists[idx].classList.add('hidden')
+            }
+            
         })
-       
-        btnItem.classList.add('feature__link_active')
-        
-        lists.forEach((listItem) => {
-            listItem.classList.add('hidden')
-        })
-
-        lists[index].classList.remove('hidden')
-
     })
 })
