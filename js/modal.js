1e9a7293c69614c31e096d65c921ecad3aa634d8
lukashevich-vitaly <lykashevich.vitalij@gmail.com>
2021-09-23 22:18:11 +0300

    feat: add burger-menu

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,5 @@
 const moreBtn = document.querySelector(".more")
 const modal = document.querySelector(".modal")
-console.log(moreBtn);
-console.log(modal);
 
 moreBtn.addEventListener('click', () => modal.classList.remove("hidden"))
 modal.addEventListener('click', (event) => {
